e1d19b7d035fe87bdf64676f6776a2a32a793849
Matias Serantes <matiasfs99@protonmail.com>
2024-03-05 21:42:06 -0300

    Second tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Assets, AssetsManifest, Sprite } from 'pixi.js'
+import { Application, Assets, AssetsManifest, Container, Sprite } from 'pixi.js'
 
 
 
@@ -12,6 +12,25 @@ const app = new Application<HTMLCanvasElement>({
 	width: 640,
 	height: 480
 });
+
+window.addEventListener("resize", ()=>{
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX,scaleY);
+	
+	const gameWidth = Math.floor(app.screen.width * scale);
+	const gameHeight = Math.floor(app.screen.height * scale);
+
+	const marginHorizonal = Math.floor((window.innerWidth - gameWidth)/2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight)/2);
+
+	app.view.style.width = gameWidth+"px";
+	app.view.style.height = gameHeight+"px";
+	app.view.style.marginLeft = marginHorizonal+"px";
+	app.view.style.marginTop = marginVertical+"px";
+
+});
+
 async function init() {
 	const manifest:AssetsManifest  = {
 		bundles: [
@@ -28,7 +47,7 @@ async function init() {
     // Assets.init must only happen once! 
     // Pack all your bundles into one manifest!
     await Assets.init({ manifest: manifest });
-
+	window.dispatchEvent(new Event('resize'));
     DrawScreen(); 
 }
 
@@ -41,18 +60,31 @@ async function DrawScreen(){
 	const crab: Sprite = Sprite.from("ConitoCrab");
 	
 	console.log(clampy.width + " " + clampy.height);
-	clampy.anchor.set(0);
 	
-	clampy.x = app.screen.width / 4;
-	clampy.y = app.screen.height / 4;
+	crab.x = 0;
+	crab.y = 0;
+	crab.anchor.set(0.5);
+	crab.scale.set(0.75);
+	crab.zIndex = 1;
+
+	clampy.anchor.set(0.5);
+	clampy.x = crab.x+190;
+	clampy.y = crab.y+90;
 	clampy.scale.set(0.25);
+	clampy.zIndex = 2;
 
-	crab.x = (app.screen.width / 4)*2;
-	crab.y = (app.screen.height / 4)*2;
-	crab.scale.set(0.25);
+	const crabWithClamp: Container = new Container();
+	crabWithClamp.sortableChildren = true;
 
-	app.stage.addChild(clampy);
-	app.stage.addChild(crab);
+	crabWithClamp.addChild(clampy);
+	crabWithClamp.addChild(crab);
+
+	crabWithClamp.x = app.screen.width-(app.screen.width / 2);
+	crabWithClamp.y = app.screen.height-(app.screen.height / 2);
+	
+	crabWithClamp.angle = -15;
+
+	app.stage.addChild(crabWithClamp);
 }
 
 
